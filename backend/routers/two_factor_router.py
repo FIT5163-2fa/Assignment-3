aba21c18d47195d4549e093388f423ddf1b51f44
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-05-13 03:20:56 +1000

    Moved generating totp to private function, and overhauled apis

--- backend/routers/two_factor_router.py
+++ backend/routers/two_factor_router.py
@@ -31,8 +31,27 @@ class GetTOTPResponse(BaseModel):
     totp_code: int
 
 
+def _generate_totp(user):
+    # HMAC Hash
+    digest = hmac.HMAC(user.two_factor_secret, hashes.SHA256())
+    # Calculate Counter floor(unix_time/totp_duration)
+    counter = floor(int(time.time()) / TOTP_DURATION_SEC)
+    # Format hash output as a 8 byte big-endian number
+    digest.update(counter.to_bytes(8, "big"))
+    hash = digest.finalize()
+
+    # Get last 4 bits to get offset
+    lsb = hash[-1] & 0b1111
+    # Calculate Offset
+    offset = hash[lsb : lsb + 4]
+    # Mask off top bit
+    offset = int.from_bytes(offset, "big") & 0x7FFFFFFF
+
+    return offset % 10**6
+
+
 @two_factor_router.get(
-    "/get_2fa_key",
+    "/DEBUG_get_2fa_key",
     description="Returns the TOTP password for a given user_id",
     responses={
         404: {"description": "User or 2fa secret not found"},
@@ -44,23 +63,23 @@ def get_totp_code(user_id: int, db: Session = Depends(get_db)) -> int:
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     if user.two_factor_secret:
-        # HMAC Hash
-        digest = hmac.HMAC(user.two_factor_secret, hashes.SHA256())
-        # Calculate Counter floor(unix_time/totp_duration)
-        counter = floor(int(time.time()) / TOTP_DURATION_SEC)
-        # Format hash output as a 8 byte big-endian number
-        digest.update(counter.to_bytes(8, "big"))
-        hash = digest.finalize()
+        return GetTOTPResponse(totp_code=_generate_totp(user))
+    else:
+        raise HTTPException(status_code=404, detail="No two factor secret not found")
 
-        # Get last 4 bits to get offset
-        lsb = hash[-1] & 0b1111
-        # Calculate Offset
-        offset = hash[lsb : lsb + 4]
-        # Mask off top bit
-        offset = int.from_bytes(offset, "big") & 0x7FFFFFFF
 
-        # TOTP Code mod to mask only needed number of digits
-        return GetTOTPResponse(totp_code=offset % 10**6)
+@two_factor_router.get(
+    "/validate_2fa_key",
+    description="Checks if supplied 2fa code is correct",
+)
+def validate_2fa_key(
+    user_id: int, user_totp: int, db: Session = Depends(get_db)
+) -> bool:
+    user = get_user(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    if user.two_factor_secret:
+        return user_totp == _generate_totp(user)
     else:
         raise HTTPException(status_code=404, detail="No two factor secret not found")
 
